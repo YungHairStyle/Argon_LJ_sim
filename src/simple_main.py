25fa7520015de98115e5a9d77c56e25aa93bcc4b
Asaad Hanna <asaad@Asaads-MacBook-Air.local>
2025-11-28 17:07:51 -0500

    added simple main

---
+++ src/simple_main.py
@@ -0,0 +1,14 @@
+from LJ import LJSimulation
+from analyze import Analysis
+
+MODE = "bulk"   # or "slab"
+
+mode = MODE.lower()
+
+# 1) Run MD
+sim = LJSimulation(mode=mode)
+sim.run()
+
+# 2) Run analysis
+analyzer = Analysis()
+analyzer.run_all()
